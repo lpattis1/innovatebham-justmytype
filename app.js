59b5f6165a5fdaea7f1877df0f9b4bbdf460a710
lpattis1 <lpattis1@asu.edu>
2021-09-14 10:42:20 -0500

    Fixed errors, added retry button to last sentence.

--- app.js
+++ app.js
@@ -11,14 +11,20 @@ const sentences = [
   "itant eate anot eat nato inate eat anot tain eat",
   "nee ene ate ite tent tiet ent ine ene ete ene ate",
 ];
+
 const upperCaseKeyboard = $("#keyboard-upper-container");
 const lowerCaseKeyboard = $("#keyboard-lower-container");
 const sentenceContainer = $("#sentence");
 const feedback = $("#feedback");
+let yellowBlock = $("#yellow-block");
 
 let sentenceIndex = 0;
 let letterIndex = 0;
-let yellowBlock = $("#yellow-block");
+let mistakes = 0;
+let timer = 0;
+let timeStart;
+let timeEnd;
+let gameEnd = false;
 
 const typingArea = $("#target-letter");
 
@@ -38,6 +44,12 @@ $(document).ready(function (e) {
 
 function displayTargetedKey(e) {
   $(document).keypress(function (e) {
+    timer++;
+
+    timeStart = e.timeStamp;
+    console.log(timeStart);
+
+    console.log(timer);
     const showTyping = String.fromCharCode(e.keyCode);
     const keyNum = e.which;
     const selectedKey = $(`#${keyNum}`);
@@ -47,13 +59,13 @@ function displayTargetedKey(e) {
     }, 500);
     typingArea.text(showTyping);
 
-    currentLetterCheck(showTyping, keyNum, selectedKey);
+    currentLetterCheck(showTyping);
   });
 }
 
 function changeToUpperCase() {
   $(document).keydown(function (e) {
-    const showTyping = String.fromCharCode(e.keyCode);
+    const showTyping = e.keyCode;
     const keyNum = e.which;
 
     if (keyNum === 16) {
@@ -76,9 +88,11 @@ function changeToLowerCase() {
   });
 }
 
-function currentLetterCheck(key, num, keyID) {
-  let currentSentence = sentences[sentenceIndex].split("");
-  let checked = currentSentence[letterIndex++].charAt();
+function currentLetterCheck(key) {
+  let currentSentence = sentences[sentenceIndex];
+  let checked = currentSentence.charAt(letterIndex++);
+  console.log(checked);
+  console.log(key);
 
   yellowBlock.css({
     left: "+=17.2px",
@@ -90,20 +104,27 @@ function currentLetterCheck(key, num, keyID) {
     feedback.append(correct);
   } else {
     let incorrect = $(`<i class="fas fa-times incorrect"></i>`);
+    mistakes++;
     incorrect.addClass("red-check");
     feedback.append(incorrect);
   }
 
-  if (letterIndex === currentSentence.length) {
-    feedback.html("");
+  if (letterIndex === currentSentence.length && sentenceIndex !== 4) {
+    letterIndex = 0;
     yellowBlock.css({
       left: "0",
     });
+    feedback.html("");
     sentenceIndex++;
-
-    letterIndex = 0;
-    currentSentence = sentences[sentenceIndex].split("");
+    currentSentence = sentences[sentenceIndex];
 
     sentenceContainer.text(sentences[sentenceIndex]);
+  }
+
+  if (letterIndex > currentSentence.length) {
+    yellowBlock.css({
+      left: "0",
+    });
+    feedback.html(`<button class="retry-btn">Retry?</button>`);
   }
 }
